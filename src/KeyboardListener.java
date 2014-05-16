0a368e97c3c9ad6ece1e8e4b773525ea3ceb63ce
student <student@192.168.117.133>
2014-05-15 20:13:07 -0400

    Fixed Magic numbers in keyboard listener

--- src/KeyboardListener.java
+++ src/KeyboardListener.java
@@ -5,26 +5,27 @@ import java.awt.event.KeyEvent;
  	
  		public void keyPressed(KeyEvent e){
  		    switch(e.getKeyCode()){
-		    	case 39:	// -> Right 
+		    	case KeyEvent.VK_RIGHT:
 		    				//if it's not the opposite direction
 		    				if(ThreadsController.directionSnake!=2) 
 		    					ThreadsController.directionSnake=1;
 		    			  	break;
-		    	case 38:	// -> Top
+		    	case KeyEvent.VK_UP:
 							if(ThreadsController.directionSnake!=4) 
 								ThreadsController.directionSnake=3;
 		    				break;
 		    				
-		    	case 37: 	// -> Left 
+		    	case KeyEvent.VK_LEFT: 
 							if(ThreadsController.directionSnake!=1)
 								ThreadsController.directionSnake=2;
 		    				break;
 		    				
-		    	case 40:	// -> Bottom
+		    	case KeyEvent.VK_DOWN:	// -> Bottom
 							if(ThreadsController.directionSnake!=3)
 								ThreadsController.directionSnake=4;
 		    				break;
-		    	
+		    	case KeyEvent.VK_ESCAPE:   
+		    		        
 		    	default: 	break;
  		    }
  		}
